c6ea6b6c47392ecb657362a80befec80cd21f3b1
lowkbyza <43971089+lowkbyza@users.noreply.github.com>
2018-11-03 14:30:39 +0700

    Update Rasta-OCS.sh

--- Rasta-OCS.sh
+++ Rasta-OCS.sh
@@ -142,8 +142,7 @@ apt-get -y install zip unzip
 cd /home/vps/public_html
 
 #wget https://github.com/rasta-team/Full-OCS/raw/master/panelocs.zip
-wget https://github.com/lowkbyza/li/raw/master/public_html.zip
-
+wget https://github.com/lowkbyza/li/blob/master/panel.zip
 mv public_html.zip LTEOCS.zip
 
 unzip LTEOCS.zip
